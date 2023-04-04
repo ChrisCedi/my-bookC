68fb9f5eed3078fc91fd4877b69916899a464fd6
ChrisCedi <christian.cedillo.palacios@outlook.com>
2023-04-04 16:49:05 -0600

    feature: animation in image

--- src/views/Error404/Error404.js
+++ src/views/Error404/Error404.js
@@ -3,6 +3,7 @@ import { Images } from "../../assets/Images";
 import { useStyles } from "./Error404Styles";
 import { Typography, Button } from "@material-ui/core";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 export const Error404 = () => {
   const classes = useStyles();
@@ -10,7 +11,13 @@ export const Error404 = () => {
 
   return (
     <div className={classes.container}>
-      <img src={Images.ruteError} alt="ruteError" className={classes.image} />
+      <motion.div
+        className={classes.cardProject}
+        whileHover={{ scale: 1.1 }}
+        transition={{ type: "spring", stiffness: 300, damping: 1 }}
+      >
+        <img src={Images.ruteError} alt="ruteError" className={classes.image} />
+      </motion.div>
       <Typography variant="h5" className={classes.description}>
         Ruta no encontrada
       </Typography>
