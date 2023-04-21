3feaac43467755288d3f21109923b9f0d92841e6
ChrisCedi <christian.cedillo.palacios@outlook.com>
2023-04-21 10:42:06 -0600

    feature: styles in mainSection

--- src/components/MainSection/MainSection.js
+++ src/components/MainSection/MainSection.js
@@ -1,46 +1,94 @@
 import React from "react";
 import { useStyles } from "./MainSectionStyles";
 import { motion } from "framer-motion";
-import { Typography, IconButton, Box } from "@material-ui/core";
+import { Typography, IconButton, Box, withWidth } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
-export const MainSection = () => {
+const MainSection = ({ width }) => {
   const classes = useStyles();
 
   return (
     <section className={classes.container} id="mainSection">
       <Box>
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            duration: 0.8,
-            delay: 0.5,
-            ease: [0, 0.71, 0.2, 1.01],
-          }}
-          className={classes.motionDiv}
-        >
-          <Typography variant="h2" color="primary" className={classes.name}>
-            Christian Cedillo
-          </Typography>
-          <Typography
-            variant="h4"
-            color="secondary"
-            className={classes.subtitle}
+        {width === "xs" ? (
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.8,
+              delay: 0.5,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
+            className={classes.motionDiv}
           >
-            Frontend Developer
-          </Typography>
-          <IconButton
-            onClick={() =>
-              document.getElementById("aboutMe").scrollIntoView({
-                behavior: "smooth",
-              })
-            }
+            <Typography
+              variant="h5"
+              color="primary"
+              className={classes.nameP}
+              style={{ fontWeight: 600, letterSpacing: "8px" }}
+            >
+              Christian Cedillo
+            </Typography>
+            <Typography
+              variant="h5"
+              color="secondary"
+              key="12312"
+              className={classes.subtitleP}
+              style={{ fontWeight: 600, letterSpacing: "8px" }}
+            >
+              Frontend Developer
+            </Typography>
+            <IconButton
+              onClick={() =>
+                document.getElementById("aboutMe").scrollIntoView({
+                  behavior: "smooth",
+                })
+              }
+            >
+              <ExpandMoreIcon className={classes.iconButton} fontSize="large" />
+            </IconButton>
+          </motion.div>
+        ) : (
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.8,
+              delay: 0.5,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
+            className={classes.motionDiv}
           >
-            <ExpandMoreIcon className={classes.iconButton} fontSize="large" />
-          </IconButton>
-        </motion.div>
+            <Typography
+              variant="h2"
+              color="primary"
+              className={classes.nameP}
+              style={{ fontWeight: 600, letterSpacing: "8px" }}
+            >
+              Christian Cedillo
+            </Typography>
+            <Typography
+              variant="h4"
+              color="secondary"
+              className={classes.subtitleP}
+              style={{ fontWeight: 600, letterSpacing: "8px" }}
+            >
+              Frontend Developer
+            </Typography>
+            <IconButton
+              onClick={() =>
+                document.getElementById("aboutMe").scrollIntoView({
+                  behavior: "smooth",
+                })
+              }
+            >
+              <ExpandMoreIcon className={classes.iconButton} fontSize="large" />
+            </IconButton>
+          </motion.div>
+        )}
       </Box>
     </section>
   );
 };
+
+export default withWidth()(MainSection);
